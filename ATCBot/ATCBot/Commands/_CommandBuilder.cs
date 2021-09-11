d792b620438aba6dc465ce98a5bf31cf592a1c4d
Shadow <shadowtail117@gmail.com>
2021-09-11 15:13:08 -0500

    Add a lot of stuff

---
+++ ATCBot/ATCBot/Commands/_CommandBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Net;
+
+namespace ATCBot
+{
+    partial class Program
+    {
+        public async Task BuildCommands()
+        {
+            if(config.shouldBuildCommands)
+            {
+                await Log(new LogMessage(LogSeverity.Debug, "Slash Command Builder", "Building slash commands..."));
+                config.shouldBuildCommands = false;
+            }
+            else
+            {
+                await Log(new LogMessage(LogSeverity.Debug, "Slash Command Builder", "Skipping building slash commands..."));
+                return;
+            }
+
+            var commands = new List<SlashCommandBuilder>();
+
+            StartProcessing();
+            StopProcessing();
+            Version();
+
+            try
+            {
+                foreach(var command in commands)
+                    await client.CreateGlobalApplicationCommandAsync(command.Build());
+            }
+            catch (ApplicationCommandException e)
+            {
+                await Log(new LogMessage(LogSeverity.Critical, "Slash Command Builder", "Could not initialize a slash command!", e));
+                throw;
+            }
+
+            void StartProcessing()
+            {
+                var startCommand = new SlashCommandBuilder();
+                startCommand.WithName("start");
+                startCommand.WithDescription("Start updating the status channel. Admins only.");
+                commands.Add(startCommand);
+            }
+
+            void StopProcessing()
+            {
+                var stopCommand = new SlashCommandBuilder();
+                stopCommand.WithName("stop");
+                stopCommand.WithDescription("Stop updating the status channel. Admins only.");
+                commands.Add(stopCommand);
+            }
+
+            void Version()
+            {
+                var versionCommand = new SlashCommandBuilder();
+                versionCommand.WithName("version");
+                versionCommand.WithDescription("Get the version of the bot!");
+                commands.Add(versionCommand);
+            }
+        }
+    }
+}
--- ATCBot/ATCBot/Program.cs
+++ ATCBot/ATCBot/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+
+using Newtonsoft.Json;
 
 namespace ATCBot
 {
-    class Program
+    partial class Program
     {
+        internal DiscordSocketClient client;
+
+        internal static Config config = new Config();
+        private static bool shouldSaveConfig = true;
+
+        internal bool shouldUpdate = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Stuff to set up the console
+            Console.Title = "ATCBot v." + Config.version;
+            Console.WriteLine($"Booting up ATCBot version {Config.version}.");
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnExit);
+
+            if (!config.Load(out config))
+            {
+                shouldSaveConfig = false;
+                Console.WriteLine("Couldn't load config. Aborting. Press any key to exit.");
+                Console.ReadKey();
+                return;
+            }
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.Log += Log;
+            client.Ready += ClientReady;
+            client.InteractionCreated += ClientInteractionCreated;
+            //client.MessageReceived += MessageReceived;
+
+            await client.LoginAsync(TokenType.Bot, config.token);
+            await client.StartAsync();
+            await client.SetGameAsync(config.prefix + "commands");
+
+            await Task.Delay(-1);
+        }
+
+        public async Task ClientReady()
+        {
+            await BuildCommands();
+        }
+
+        /// <summary>
+        /// Logs a message. Use this over <see cref="Console.WriteLine()"/> when possible.
+        /// </summary>
+        /// <param name="message">The message to be logged.</param>
+        internal static Task Log(LogMessage message)
+        {
+            Console.ForegroundColor = message.Severity switch
+            {
+                LogSeverity.Critical => ConsoleColor.Red,
+                LogSeverity.Error => ConsoleColor.Red,
+                LogSeverity.Warning => ConsoleColor.Yellow,
+                LogSeverity.Info => ConsoleColor.White,
+                LogSeverity.Verbose => ConsoleColor.DarkGray,
+                LogSeverity.Debug => ConsoleColor.DarkGray,
+                _ => throw new ArgumentException("Invalid severity!")
+            };
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+            Console.ResetColor();
+            return Task.CompletedTask;
+        }
+
+        private static void OnExit(object sender, EventArgs e)
+        {
+            if (!shouldSaveConfig) return;
+            Console.WriteLine("\nShutting down!");
+            config.Save();
         }
     }
 }
