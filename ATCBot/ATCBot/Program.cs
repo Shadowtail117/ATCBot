a41ff4c3e52f5807c6b5c73364e64c1db9b9807a
Shadow <shadowtail117@gmail.com>
2021-09-11 13:49:42 -0500

    Initial Project - Add blank solution - Add Discord.net and Steamworks dependencies

---
+++ ATCBot/ATCBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATCBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
