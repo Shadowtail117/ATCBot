542bccfe2bbe0460d1782067b66c2a5c42dad5f0
Shadow <shadowtail117@gmail.com>
2021-09-13 01:50:27 -0500

    Assorted changes (#2)
    * Fix classes, modularize commands
    broken
    
    * Remove test command
    And add confirm save

---
+++ ATCBot/ATCBot/Commands/StopUpdating.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace ATCBot.Commands
+{
+    public class StopUpdating : Command
+    {
+        public override string Name { get; set; } = "stopupdating";
+        public override SlashCommandBuilder Builder { get; set; } = new SlashCommandBuilder()
+            .WithName("stopupdating")
+            .WithDescription("Stop updating the lobby data. Requires Manage Server permissions.");
+
+        public override string Action(SocketSlashCommand command)
+        {
+            if (command.User is SocketGuildUser u)
+            {
+                if (u.GuildPermissions.ManageGuild == true)
+                {
+                    if (!Program.shouldUpdate)
+                        return "Already not updating!";
+                    else
+                    {
+                        Program.shouldUpdate = false;
+                        return "Stopped updating!";
+                    }
+                }
+                else return "Sorry, you don't have enough permissions for this!";
+            }
+            else throw new System.Exception("Could not get user permissions!");
+        }
+    }
+}
---
+++ ATCBot/ATCBot/Commands/_Command.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+using System;
+using System.Collections.Generic;
+
+namespace ATCBot.Commands
+{
+    public abstract class Command
+    {
+        public static List<Command> AllCommands { get; set; } = new();
+
+        public abstract string Name { get; set; }
+
+        public abstract SlashCommandBuilder Builder { get; set; }
+
+        public abstract string Action(SocketSlashCommand command);
+    }
+}
