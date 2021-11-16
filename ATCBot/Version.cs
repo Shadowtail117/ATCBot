26376d0d96a2ae6cde5d3cbbf8bccbb4215a25ab
Shadow <shadowtail117@gmail.com>
2021-11-16 16:45:38 -0600

    Version now uses external gist

--- ATCBot/Version.cs
+++ ATCBot/Version.cs
@@ -12,14 +12,14 @@ namespace ATCBot
         /// <summary>
         /// The local version of the bot obtained from version.txt.
         /// </summary>
-        public static string LocalVersion { get; } = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "version.txt"));
+        public static string LocalVersion { get; } = "1.4.0p5";
 
         /// <summary>
         /// The remote version on the repository.
         /// </summary>
         public static string RemoteVersion { get; private set; }
 
-        private static readonly string url = "https://raw.githubusercontent.com/Shadowtail117/ATCBot/releases/ATCBot/version.txt";
+        private static readonly string url = "https://gist.githubusercontent.com/Shadowtail117/507f77becaa1ea91a0caf6d1eca2e0ec/raw/c693ca7be928653c4f40c6d8b03d298f83cae13e/version.txt";
 
         /// <summary>
         /// Retrieve the remote version stored on the repository and verify it matches the local version.
